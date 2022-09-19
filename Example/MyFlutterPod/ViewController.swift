629d05c10d8c219a6c0b5eba6811726ee7126b15
zhangkaiqiang <1510166838@qq.com>
2022-09-19 16:25:44 +0800

    Initial commit

---
+++ Example/MyFlutterPod/ViewController.swift
@@ -0,0 +1,24 @@
+//
+//  ViewController.swift
+//  MyFlutterPod
+//
+//  Created by 151016zkq on 09/19/2022.
+//  Copyright (c) 2022 151016zkq. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+}
+
